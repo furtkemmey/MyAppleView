ebded3aca6902eb49ddd07a6e8e3bea71d98e876
KaiChieh <KaiChieh@yvtc.edu.tw>
2018-03-21 16:30:40 +0800

    done 3/21

--- MyAppleView/ViewController.swift
+++ MyAppleView/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    let localManager = CLLocationManager()
+
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        localManager.requestAlwaysAuthorization()
+        localManager.delegate = self
+        mapView.delegate = self
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 24.137426, longitude: 121.275753)
+        annotation.title = "武嶺"
+        annotation.subtitle = "仁愛鄉"
+        mapView.showsUserLocation = true
+        mapView.addAnnotation(annotation)
+        mapView.setCenter(annotation.coordinate, animated: false)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        localManager.startUpdatingLocation()
+    }
+}
+extension ViewController: CLLocationManagerDelegate, MKMapViewDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        <#code#>
+    }
 
 }
 
